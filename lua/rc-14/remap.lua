f818ddc11a66057cc3b90b9680a45ceea6a4e681
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-12-19 18:19:48 +0100

    Add keymaps for system clipboard

--- lua/rc-14/remap.lua
+++ lua/rc-14/remap.lua
@@ -1,3 +1,9 @@
+-- Copy/Paste system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y')
+vim.keymap.set({'n', 'v'}, '<leader>p', '"+p')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+vim.keymap.set({'n', 'v'}, '<leader>P', '"+P')
+
 -- Open netrw
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 

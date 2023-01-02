24d8ad1f00cc078111de12d833b0bd2f295eb25c
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-01-02 04:24:37 +0100

    Good completion controlsw

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,6 +10,19 @@ lsp.ensure_installed({
 	'sumneko_lua',
 })
 
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['<C-y>'] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings
+})
+
 lsp.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 

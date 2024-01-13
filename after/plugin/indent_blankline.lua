354282eedcd9b16ec029c5cbdf098935ca264e4d
RC-14 <61058098+RC-14@users.noreply.github.com>
2024-01-13 21:39:27 +0100

    Update indent_blankline config

--- after/plugin/indent_blankline.lua
+++ after/plugin/indent_blankline.lua
@@ -1,6 +1,7 @@
-require('indent_blankline').setup {
-	['use_treesitter'] = true,
-	['use_treesitter_scope'] = true,
-	['show_first_indent_level'] = false,
-	['show_current_context'] = true,
+require('ibl').setup {
+	scope = {
+		enabled = true
+	},
+	-- ['show_first_indent_level'] = false,
+	-- ['show_current_context'] = true,
 }
